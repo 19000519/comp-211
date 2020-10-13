22249541c1ae8638d8f13078a0cc7a2d8ee5ba6f
KOSWANE PHEMELO <KP19000519@BIUST.AC.BW>
2020-10-13 14:42:06 +0200

    commit factorial.c

--- factorial.c
+++ factorial.c
@@ -1,2 +1,25 @@
 #include<stdio.h>
 #include<conio.h>
+
+//author : ppk
+// purpose : To read a number and print its factorial
+
+ void main()
+ {
+     //variable defination
+     int input, i, fact = 1;
+
+     //read data from user
+     printf("Enter any number:");
+     scanf("%d", &input);
+
+     //logic
+     for (i = 1; i <= input; i++)
+        fact = fact*i;
+
+     //print the result
+     printf("Factorial of %d is %d", input, fact);
+     
+     getch();
+    
+ }
